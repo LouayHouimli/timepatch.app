55d00cc36667c82fec2c9b2e088816e4067d41ef
LouayHouimli <louayhouimli@engineer.com>
2025-05-03 19:05:40 +0100

    add cf types

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,9 +3,9 @@ import { trpc } from "@/utils/trpc";
 import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
-  const healthCheck = useQuery(trpc.healthCheck.queryOptions());
+	const healthCheck = useQuery(trpc.healthCheck.queryOptions());
 
-  return (
-    <div className="container mx-auto max-w-3xl px-4 py-2">Time Patch v1</div>
-  );
+	return (
+		<div className="container mx-auto max-w-3xl px-4 py-2">Time Patch v1</div>
+	);
 }
